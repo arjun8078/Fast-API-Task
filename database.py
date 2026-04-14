b6f9a552a83dfce9f2248eb1a6ae764f10251606
arjun8078 <arjunkanhangad143@gmail.com>
2026-04-14 14:17:09 +0530

    CI pipeline added

--- database.py
+++ database.py
@@ -9,6 +9,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+else:
+    engine = None  # prevents crash in CI
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
